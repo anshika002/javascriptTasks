caa5dd2e92431098d4e53a7687c1bece926e694d
anshika002 <anshikapareek4@gmail.com>
2023-03-21 09:33:11 +0530

    some commit

--- src/screen/index.js
+++ src/screen/index.js
@@ -1,61 +1,58 @@
+let numDisplayed = 20;
 function createCard() {
-	const card = document.createElement("div");
-	card.classList.add("card");
+	const card1 = document.createElement("div");
+	card1.classList.add("card");
 	// add card content here based on the data object
-	return card;
+	return card1;
   
   };
-function posts() {
-	fetch("//jsonplaceholder.typicode.com/posts")
-					.then((data) =>{
-						return data.json();
-					}).then((completedata) =>{
-						//console.log(completedata[2].title);
-						let data1 ="";
-						completedata.map((values)=>{
-							data1+=`<div class "card"  style="background-color:white;width: 14rem;height: 16rem; >
-							<h1 class="title">${values.title}</h1>
-							<p>${values.body}</p>
-							
-						</div>`
-						});
-						document.getElementById("cards").innerHTML=data1;
-					}).catch((err)=>{
-						console.log(err);
-					});
-createCard();
-	
-}
-
-posts();
-
-
-document.getElementById("postbutton").addEventListener("click",function(){
-	posts();})
-		
-document.getElementById("commentbutton").addEventListener("click",function(){
-			fetch("//jsonplaceholder.typicode.com/comments")
-				.then((data) =>{
-					return data.json();
-				}).then((completedata) =>{
-					//console.log(completedata[2].title);
-					let data1 ="";
-					completedata.map((values)=>{
-						data1+=`<div class "card" style="background-color:white;width: 14rem;height: 13rem;>
-						<h1 class="title">${values.name}</h1>
-						<p>${values.email}</p>
-						<p>${values.body}</p>
-						
-					</div>`
-					});
-					document.getElementById("cards").innerHTML=data1;
-				}).catch((err)=>{
-					console.log(err);
-				});
- createCard();
-			}
-		)
-			
-		
-		
-		
+function displayCards(url, titleKey, bodyKey, nameKey, emailKey, numToDisplay) {
+    fetch(url)
+      .then((data) => data.json())
+      .then((completedata) => {
+        let data1 = "";
+        completedata.slice(0, numToDisplay).forEach((values) => {
+          const card = createCard();
+          card.innerHTML = `<div class "card" style="background-color:white;width: 14rem;height: 13rem;>
+            <h1 class="title" >${values[titleKey] || values[nameKey]}</h1>
+            <p>${values[bodyKey] || values[emailKey] || ""}</p>
+          `;
+          data1 += card.outerHTML;
+        });
+        document.getElementById("cards").apendChild(data1);
+      })
+      .catch(console.log);
+  }
+  // document.getElementById("postbutton").addEventListener("click", () => {
+  //   numDisplayed += 20;
+  //   displayCards("//jsonplaceholder.typicode.com/posts", "title", "body", "", "", numDisplayed);
+  // });
+  
+  // document.getElementById("commentbutton").addEventListener("click", () => {
+  //   numDisplayed += 20;
+  //   displayCards("//jsonplaceholder.typicode.com/comments", "", "body", "name", "email", numDisplayed);
+  // });
+  document.getElementById("loadmore").addEventListener("click", () => {
+    numDisplayed += 20;
+    const postButton = document.getElementById("postbutton");
+    const commentButton = document.getElementById("commentbutton");
+    if (postButton.classList.contains("active")) {
+      displayCards("//jsonplaceholder.typicode.com/posts", "title", "body", "", "", numDisplayed);
+    } else if (commentButton.classList.contains("active")) {
+      displayCards("//jsonplaceholder.typicode.com/comments", "", "body", "name", "email", numDisplayed);
+    }
+  });
+  document.getElementById("postbutton").addEventListener("click", () => {
+    numDisplayed = 20;
+    displayCards("//jsonplaceholder.typicode.com/posts", "title", "body", "", "", numDisplayed);
+    document.getElementById("postbutton").classList.add("active");
+    document.getElementById("commentbutton").classList.remove("active");
+  });
+  
+  document.getElementById("commentbutton").addEventListener("click", () => {
+    numDisplayed = 20;
+    displayCards("//jsonplaceholder.typicode.com/comments", "", "body", "name", "email", numDisplayed);
+    document.getElementById("postbutton").classList.remove("active");
+    document.getElementById("commentbutton").classList.add("active");
+  });
+  document.getElementById("postbutton").click();
